d69a3ba242dbb6f4e1aa5480f7fe0c390594ef21
dabeii <tranlan0310@gmail.com>
2023-05-05 00:30:26 +0700

    some upgrade

--- src/server.js
+++ src/server.js
@@ -3,18 +3,32 @@ require('dotenv').config();
 //Khai bao express
 const express = require('express');
 const app = express();
-const http = require('http');
+const http = require('http').createServer(app);
 const cookieParser = require('cookie-parser')
+//socket io
+const io = require('socket.io')(http);
 import configViewengine from './config/viewengine.js';
 import initWebRoute from './route/web.js';
 //connect2MYSQL
 import connection from './config/connect2MySQL.js';
+import { Socket } from 'dgram';
 
+io.on('connection', function (socket) {
+    console.log('user connected');
+    socket.on('sendmsg', function (msg) {
+        console.log(msg);
+        io.emit("sendmsg", msg);
+    })
+    socket.on('sendname', function (msg) {
+        console.log(msg);
+    })
+})
 //Chay server
 const PORT = process.env.PORT;
-app.listen(PORT || 9000, () => {
+http.listen(PORT || 9000, () => {
     console.log(`Server is running on PORT: ${PORT}`);
 })
+
 //Body-parser Lay du lieu tu body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
